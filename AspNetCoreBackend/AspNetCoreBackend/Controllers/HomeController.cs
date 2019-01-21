2e138e63bf9abf1484711f18b048c89f5f376cef
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-01-21 11:38:27 +0200

    Lisätty projektin Entity-mallit.

--- AspNetCoreBackend/AspNetCoreBackend/Controllers/HomeController.cs
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/HomeController.cs
@@ -39,5 +39,11 @@ namespace AspNetCoreBackend.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public IActionResult OmaJuttu()
+        {
+            return View();
+        }
     }
 }
